ac413bf704f21e3c21853867f3ce50781baefd87
Said <saidazimsaidmuratov@gmail.com>
2025-05-24 00:51:20 +0500

    dasd

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,16 +1,16 @@
 export default function QuestionCard({ question, onAnswer, index, total }) {
   return (
-    <div className="bg-white p-6 rounded-xl shadow-md space-y-6">
+    <div className="bg-black p-6 shadow-md space-y-6 text-white">
       <h2 className="text-xl font-semibold">
         Question {index + 1} of {total}
       </h2>
-      <p className="text-gray-700">{question.question}</p>
+      <p className="text-black-700">{question.question}</p>
       <div className="space-y-3">
         {question.options.map((option, i) => (
           <button
             key={i}
             onClick={() => onAnswer(i)}
-            className="block w-full text-left px-4 py-3 rounded bg-blue-100 hover:bg-blue-200 transition"
+            className="block w-full text-left px-4 py-3 text-black  bg-white hover:bg-black hover:text-white transition"
           >
             {option}
           </button>
